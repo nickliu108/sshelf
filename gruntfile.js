c61db5fac2518f02de3cedccade4cfcfb56a7591
l.liu <lidan.liu@net-a-porter.com>
2016-03-09 21:07:25 +0000

    add grunt concate job

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,25 @@
 'use strict';
 
-module.exports = function(){
+module.exports = function(grunt){
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['public/javascripts/magazine/*.js'],
+        // the location of the resulting JS file
+        dest: 'dist/<%= pkg.targetPath %>.js'
+      }
+    }
+  });
+
+  grunt.registerTask('build', [
+    'concat'
+  ]);
 };
